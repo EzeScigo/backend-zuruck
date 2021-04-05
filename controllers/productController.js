e0d0b3820c757ecbe5deb9dd09dd86ab94338a49
EzeScigo <scigolini@gmail.com>
2021-04-05 22:18:10 +0200

    mm

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,7 @@ import Product from '../models/productModel.js';
 // import expressAsyncHandler from 'express-async-handler';
 // import { errorHandler } from '../helpers/dbErrorHandler.js';
 
-export const ProductList = async (req, res) => {
+const ProductList = async (req, res) => {
   let order = req.query.order ? req.query.order : 'asc';
   let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
   let limit = req.query.limit ? req.query.limit : 6;
@@ -20,3 +20,5 @@ export const ProductList = async (req, res) => {
     //   message: err.message
   }
 }
+
+export default ProductList;
