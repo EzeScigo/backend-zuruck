0251f73f8e275436da49b033631ef8777234777a
EzeScigo <scigolini@gmail.com>
2021-04-05 19:02:01 +0200

    ad

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 import Product from '../models/productModel.js';
-import expressAsyncHandler from 'express-async-handler';
-import { errorHandler } from '../helpers/dbErrorHandler.js';
+// import expressAsyncHandler from 'express-async-handler';
+// import { errorHandler } from '../helpers/dbErrorHandler.js';
 
 export const ProductList = async (req, res) => {
   let order = req.query.order ? req.query.order : 'asc';
